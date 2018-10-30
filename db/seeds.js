a66a51c2ba043e210c34c73718d573b5b837c9ef
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-10-30 07:33:12 -0400

    required models in seeds

--- db/seeds.js
+++ db/seeds.js
@@ -1,3 +1,8 @@
-const Friend = require('../models/Friend')
+const Category = require('../models/Category.js')
+const Product = require('../models/Product.js')
+const Subcategory = require('../models/Subcategory.js')
+const User = require('../models/User.js')
 const mongoose = require('./connections')
+
+
 
