e42a2a4bf48e89632c5053a9a6c2c2665c1ee3f9
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-10-30 07:30:45 -0400

    added subcategory object within category object

--- models/Category.js
+++ models/Category.js
@@ -2,7 +2,13 @@ const mongoose = require('../db/connections')
 const Schema = mongoose.Schema
 
 const Category = new Schema ({
-	name: String
+	name: String,
+	subcategories: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Subcategory'
+		}
+	]
 })
 
 module.exports = mongoose.model('Category', Category)
--- models/Subcategory.js
+++ models/Subcategory.js
@@ -2,7 +2,13 @@ const mongoose = require('../db/connections')
 const Schema = mongoose.Schema
 
 const Subcategory = new Schema ({
-	name: String
+	name: String,
+	products: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Product'
+		}
+	]
 })
 
 module.exports = mongoose.model('Subcategory', Subcategory)
