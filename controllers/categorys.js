4155f179d2f55e267cb6380eebce0e1a35b50c5d
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-10-30 04:20:17 -0400

    created categorys controller

--- controllers/categorys.js
+++ controllers/categorys.js
@@ -1,5 +1,9 @@
 const Category = require('../models/Category')
 
 const categorysController = ({
-	
+	index: (req, res) => {
+		res.render('categorys/index')
+	}
 })
+
+module.exports = categorysController
