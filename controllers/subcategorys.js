89258d5755ac033ca9ad2b7ab4b6097602df8e68
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-10-30 05:18:10 -0400

    removed category variable from subcategory controller

--- controllers/subcategorys.js
+++ controllers/subcategorys.js
@@ -1,9 +1,12 @@
-const Category = require('../models/Category')
 const Subcategory = require('../models/Subcategory')
 
 const subcategorysController = {
 	show: (req, res) => {
-		
+		const subcategoryId = req.params.subcateorysId
+		Subcategory.findById(subcategoryId)
+		.then(subcategory => {
+			res.render('subcategorys/show', { subcategory: subcategory })
+		})
 	}
 }
 
